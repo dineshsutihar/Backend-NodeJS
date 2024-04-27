dfc79635ec7ba7b3133556a96d48348c9e62ffc9
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-04-27 10:43:16 +0530

    Add: Practise: Partialy complete a project with multiple functionalities

---
+++ 06Practice/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const fs = require("fs");
+
+
+app.set('view engine','ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res)=>{
+    fs.readdir(`./files`,(err,files)=>{
+        res.render('index',{filesDetails:files})
+    })
+})
+
+app.post('/create',(req,res)=>{
+    //console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect('/')
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("running at 3000");
+})

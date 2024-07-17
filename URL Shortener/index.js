5f5a4cfa9953a10840b7033a88acec62b4581572
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-17 11:56:31 +0530

    feat: adds analytics and redirect api end point and also full functional working backend

--- URL Shortener/index.js
+++ URL Shortener/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const URL = require("./models/url");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connect");
 
@@ -10,6 +11,24 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("connected to mongo db")
 );
 
+app.use(express.json());
+
 app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));

344661a2f2dfbcb66d7c3b27048357d7c0be093a
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-24 01:42:27 +0530

    blog-feat: adds hashing feature to password, and also adds signin and signup page

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -1,15 +1,25 @@
 const path = require("path");
 const express = require("express");
+const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8000;
 
+mongoose
+  .connect("mongodb://localhost:27017/blogmate")
+  .then((e) => console.log("MongoDB is connected>>>>"));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server Started at PORT: ${PORT}`);
--- BlogApp/models/user.js
+++ BlogApp/models/user.js
@@ -1,3 +1,5 @@
+const { createHmac, randomBytes } = require("node:crypto");
+
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
@@ -13,7 +15,6 @@ const userSchema = new Schema(
     },
     salt: {
       type: String,
-      required: true,
     },
     password: {
       type: String,
@@ -33,6 +34,36 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
+  const salt = randomBytes(16).toString(); // salt is like secreate password, here we are keeping different for each user
+  //   const salt = "TEst@#!for24354passwrd^*";
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
+
+userSchema.static("matchPassword", async function (email, password) {
+  const user = await this.findOne({ email });
+  if (!user) throw new Error("User Not Found!!");
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+  const userProvidedHash = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedPassword !== userProvidedHash)
+    throw new Error("Incorrect Password.");
+  return user;
+});
 
 const User = model("user", userSchema);
 module.exports = User;

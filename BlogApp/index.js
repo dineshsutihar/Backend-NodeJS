01a8303af2186ce3861c11011fce80a10bf3b5a8
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-25 19:08:45 +0530

    feat: full authentication login logout and nav name complete

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -2,6 +2,10 @@ const path = require("path");
 const express = require("express");
 const userRoute = require("./routes/user");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -14,9 +18,13 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    user: req.user,
+  });
 });
 
 app.use("/user", userRoute);
